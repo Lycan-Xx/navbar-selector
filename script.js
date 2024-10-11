c9f47ea19f7bc6d4305ab781dea7f81859a4feb7
Lycan-Xx <msbello514@gmail.com>
2024-10-11 23:55:50 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabPanes = document.querySelectorAll('.tab-pane');
+
+tabBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        tabPanes
+    })
+})
